4012b301289b5b5ef17a325b034c4133d3b8b58d
wiikmatias <matias.wiik1@gmail.com>
2022-11-03 12:31:09 +0200

    feature: add pokedex api

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+
+exports.handler = async function () {
+  const POKE_API = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+  const response = await fetch(POKE_API)
+  const data = await response.json()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+}
