ce6d4126cae3c0ad17e7fa237c8040c985416dca
wiikmatias <matias.wiik1@gmail.com>
2022-11-04 10:54:02 +0200

    feature: add add kanto and johto buttons

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,16 @@
 import fetch from 'node-fetch'
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokemon/ditto'
+export const handler = async (event, context) => {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
   const response = await fetch(POKE_API)
   const data = await response.json()
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries
+    })
   }
 }
